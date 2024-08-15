25e6c54d831a8d7a6c41c5111b7f351976018838
jawadmidani <151636540+jawadmidani@users.noreply.github.com>
2024-08-15 11:50:01 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     event.preventDefault();
     alert("Thank you for contacting RadioMK! We will get back to you soon.");
 });
+
+function updateDateTime() {
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    document.getElementById('datetime').textContent = dateTimeString;
+}
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
+updateDateTime(); // Initial call to set the date and time on load
